bed566446026e92924efdfc2dfeae6d2ee7604d6
Aditya <singhaditya9770@gmail.com>
2025-02-09 14:17:38 +0530

    task completed

---
+++ src/components/Searchicon.jsx
@@ -0,0 +1,22 @@
+
+
+
+export function Searchicon(){
+    return (
+        <svg
+      className="feather feather-search" 
+      fill="none"
+      height="20"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round" 
+      strokeWidth="1"
+      viewBox="0 0 24 24"
+      width="24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="11" cy="11" r="8" />
+      <line x1="21" y1="21" x2="16.65" y2="16.65" />
+    </svg>
+    )
+}
